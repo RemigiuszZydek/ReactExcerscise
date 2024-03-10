feb022ce7efeb378e93704669315c5f4313f59d4
Remik <zyduremik@gmail.com>
2024-03-10 21:01:52 +0100

    Update books

--- excersises/src/App.js
+++ excersises/src/App.js
@@ -6,7 +6,8 @@ function App() {
 	const [books, setBooks] = useState([]);
 
 	const createBook = (title) => {
-		console.log("Need to add book with :", title);
+		const updatedBooks = [...books, { id: 123, title }];
+		setBooks(updatedBooks);
 	};
 
 	return (
