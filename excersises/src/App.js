0374593e3c52a09a993f20e4ec38c47e9ba7087c
Remik <zyduremik@gmail.com>
2024-03-11 18:35:16 +0100

    add book showing

--- excersises/src/App.js
+++ excersises/src/App.js
@@ -1,17 +1,22 @@
 import "./App.css";
 import { useState } from "react";
 import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
 
 function App() {
 	const [books, setBooks] = useState([]);
 
 	const createBook = (title) => {
-		const updatedBooks = [...books, { id: 123, title }];
+		const updatedBooks = [
+			...books,
+			{ id: Math.round(Math.random() * 9999), title },
+		];
 		setBooks(updatedBooks);
 	};
 
 	return (
-		<div className="App">
+		<div className="app">
+			<BookList books={books} />
 			<BookCreate onSubmit={createBook} />
 		</div>
 	);
